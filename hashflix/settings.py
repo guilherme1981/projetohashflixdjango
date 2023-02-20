14e30b88432909001d59a92e06346cfe0dbcf26b
guilherme1981 <96877180+guilherme1981@users.noreply.github.com>
2023-02-20 09:57:28 -0300

    debug para True

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-so)$34g5-=uj^mmk)m+3#b#@nc6d9p97y6w-g+0--&&ize7)-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
